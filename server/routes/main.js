d75beff3170c1e67bb3c32177e2ee13ac3c36af4
wasannn <148628596+wasannn@users.noreply.github.com>
2025-01-30 19:04:07 +0300

    Update main.js

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,13 +4,13 @@ const Post = require('../models/Post');
 
 /**
  * GET /
- * HOME
+ * home 
 */
 router.get('', async (req, res) => {
   try {
     const locals = {
-      title: "NodeJs Blog",
-      description: "Simple Blog created with NodeJs, Express & MongoDb."
+      title: "Gift Me",
+      description: "Website for gifts"
     }
 
     let perPage = 10;
@@ -21,8 +21,7 @@ router.get('', async (req, res) => {
     .limit(perPage)
     .exec();
 
-    // Count is deprecated - please use countDocuments
-    // const count = await Post.count();
+    
     const count = await Post.countDocuments({});
     const nextPage = parseInt(page) + 1;
     const hasNextPage = nextPage <= Math.ceil(count / perPage);
@@ -41,25 +40,12 @@ router.get('', async (req, res) => {
 
 });
 
-// router.get('', async (req, res) => {
-//   const locals = {
-//     title: "NodeJs Blog",
-//     description: "Simple Blog created with NodeJs, Express & MongoDb."
-//   }
 
-//   try {
-//     const data = await Post.find();
-//     res.render('index', { locals, data });
-//   } catch (error) {
-//     console.log(error);
-//   }
-
-// });
 
 
 /**
  * GET /
- * Post :id
+ * post/gift :id
 */
 router.get('/post/:id', async (req, res) => {
   try {
@@ -69,7 +55,7 @@ router.get('/post/:id', async (req, res) => {
 
     const locals = {
       title: data.title,
-      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+      description: "Gift Me",
     }
 
     res.render('post', { 
@@ -86,13 +72,13 @@ router.get('/post/:id', async (req, res) => {
 
 /**
  * POST /
- * Post - searchTerm
+ * post - search by term
 */
 router.post('/search', async (req, res) => {
   try {
     const locals = {
-      title: "Seach",
-      description: "Simple Blog created with NodeJs, Express & MongoDb."
+      title: "Search",
+      description: "Gift Me"
     }
 
     let searchTerm = req.body.searchTerm;
@@ -120,7 +106,7 @@ router.post('/search', async (req, res) => {
 
 /**
  * GET /
- * About
+ * about
 */
 router.get('/about', (req, res) => {
   res.render('about', {
@@ -128,53 +114,18 @@ router.get('/about', (req, res) => {
   });
 });
 
+/**
+ * GET /
+ * contact
+*/
+router.get('/contact', (req, res) => {
+    res.render('contact', {
+      currentRoute: '/contact'
+    });
+  });
 
-// function insertPostData () {
-//   Post.insertMany([
-//     {
-//       title: "Building APIs with Node.js",
-//       body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
-//     },
-//     {
-//       title: "Deployment of Node.js applications",
-//       body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud, and container environments..."
-//     },
-//     {
-//       title: "Authentication and Authorization in Node.js",
-//       body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js or other authentication libraries."
-//     },
-//     {
-//       title: "Understand how to work with MongoDB and Mongoose",
-//       body: "Understand how to work with MongoDB and Mongoose, an Object Data Modeling (ODM) library, in Node.js applications."
-//     },
-//     {
-//       title: "build real-time, event-driven applications in Node.js",
-//       body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
-//     },
-//     {
-//       title: "Discover how to use Express.js",
-//       body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications."
-//     },
-//     {
-//       title: "Asynchronous Programming with Node.js",
-//       body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for non-blocking I/O operations."
-//     },
-//     {
-//       title: "Learn the basics of Node.js and its architecture",
-//       body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers."
-//     },
-//     {
-//       title: "NodeJs Limiting Network Traffic",
-//       body: "Learn how to limit netowrk traffic."
-//     },
-//     {
-//       title: "Learn Morgan - HTTP Request logger for NodeJs",
-//       body: "Learn Morgan."
-//     },
-//   ])
-// }
 
-// insertPostData();
+
 
 
 module.exports = router;
